d2c396f714c979883de47c4746cbbeeee6faf3b6
Bader Youssef <bader@iodlt.com>
2022-04-01 16:07:14 -0400

    added key export

--- scripts/gennetworkinfo.js
+++ scripts/gennetworkinfo.js
@@ -1,4 +1,5 @@
 import { Account, NetworkType } from 'symbol-sdk';
+import fs from 'fs'
 
 function generateAddressList(amount) {
     var accounts = [];
@@ -8,8 +9,8 @@ function generateAddressList(amount) {
     }
     const amounts = accounts.map((a, i) => {
         if (i == 0)
-            return { address: a.address.plain(), amount: 1285571428285716 }
-        return  { address: a.address.plain(), amount: 1285571428285714 }
+            return { address: a.address.plain(), amount: 1285571428285716, key: a.privateKey }
+        return  { address: a.address.plain(), amount: 1285571428285714, key: a.privateKey }
     })
 
     var setup = [ {
@@ -20,12 +21,14 @@ function generateAddressList(amount) {
     	isTransferable: true,
     	isSupplyMutable: false,
     	isRestrictable: false,
-        currencyDistributions: amounts
+        currencyDistributions: amounts.map((a) => ({address: a.address, amount: 1285571428285716 }))
     }, { repeat: 1 }]
 
     console.log('nemesis:')
     console.log('    mosaics:')
     console.log(JSON.stringify(setup, null, 2).replace(/"/g, ''))
+
+    fs.writeFileSync('keys.txt', JSON.stringify(amounts, null, 2))
 }
 
 generateAddressList(7);
